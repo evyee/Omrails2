68d79308617cba40301c9e7b6a117dd10a91cac4
evyee <jvalieva@yahoo.com>
2013-09-21 12:48:54 -0700

    Add Pins Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          #:recoverable, 
          :rememberable, :trackable, :validatable
+
+         has_many :pins, :dependent => :destroy
 end
